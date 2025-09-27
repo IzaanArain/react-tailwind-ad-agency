24d49ed0471ceec998a6c3d2753254d67a173a06
IzaanArain <izaansaquib@gmail.com>
2025-09-27 21:52:07 +0500

    chore: add new asset images and update import paths for consistency

--- src/components./reusables/navbar.tsx
+++ src/components./reusables/navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import logo from "../../assets/logo.webp";
+import logo from "/assets/logo.webp";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { LINKS } from "../../constants";
 
